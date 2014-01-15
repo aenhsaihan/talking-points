a73c03737220126b9664f838bfb551a8045c2ebc
Aditya Narayan <aditya@Adityas-iMac-9.local>
2014-01-14 23:13:19 -0500

    Initial Commit

---
+++ TalkingPoints/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TalkingPoints
+//
+//  Created by Aditya Narayan on 1/14/14.
+//  Copyright (c) 2014 TurnToTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IODAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IODAppDelegate class]));
+    }
+}
---
+++ TalkingPointsTests/TalkingPointsTests.m
@@ -0,0 +1,34 @@
+//
+//  TalkingPointsTests.m
+//  TalkingPointsTests
+//
+//  Created by Aditya Narayan on 1/14/14.
+//  Copyright (c) 2014 TurnToTech. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TalkingPointsTests : XCTestCase
+
+@end
+
+@implementation TalkingPointsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
